3fd38d520b97c7e5c5f900f02f1c8a5b09eeea98
mktakahashi <mktakahashi@gmail.com>
2019-10-26 14:37:16 -0700

    Get data for Ramen Ratings TT

--- first_try/First_script.R
+++ first_try/First_script.R
@@ -1,1 +1,9 @@
 # first tidy tuesday
+# Tidy tuesday: Ramen ratings
+#20191026
+
+##load library
+library('tidyverse')
+
+## get data
+ramen_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-04/ramen_ratings.csv")
